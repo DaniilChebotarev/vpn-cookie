11135940d1b641d2efca8a1dc8cf98a99193e833
DaniilChebotarev <119631891+DaniilChebotarev@users.noreply.github.com>
2026-04-12 21:17:06 +0300

    Update [userId].js

--- api/sub/[userId].js
+++ api/sub/[userId].js
@@ -1,12 +1,3 @@
 module.exports = async function handler(req, res) {
-  const { userId } = req.query;
-
-  const SUB_URL_1 = process.env.SERVER1_URL;
-  const SUB_URL_2 = process.env.SERVER2_URL;
-
-  res.json({ 
-    userId,
-    SUB_URL_1: SUB_URL_1 || "NOT SET",
-    SUB_URL_2: SUB_URL_2 || "NOT SET"
-  });
+  res.json(process.env);
 }
