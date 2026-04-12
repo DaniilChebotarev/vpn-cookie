e07165647ce462fa42497aa066b2d4f1f128b668
DaniilChebotarev <119631891+DaniilChebotarev@users.noreply.github.com>
2026-04-12 21:23:12 +0300

    Update [userId].js

--- api/sub/[userId].js
+++ api/sub/[userId].js
@@ -1,20 +1,21 @@
 module.exports = async function handler(req, res) {
   const { userId } = req.query;
 
-  const SUB_URL_1 = process.env.SUB1_URL;
-  const SUB_URL_2 = process.env.SUB2_URL;
+  try {
+    const [r1, r2] = await Promise.all([
+      fetch(`https://johntravolta12.mooo.com/30C70YHxj8/${userId}`),
+      fetch(`https://de-johntravolta12.mooo.com/SSwKyNnsvU/${userId}`)
+    ]);
 
-  const [r1, r2] = await Promise.all([
-    fetch(`${SUB_URL_1}/${userId}`),
-    fetch(`${SUB_URL_2}/${userId}`)
-  ]);
+    const [t1, t2] = await Promise.all([r1.text(), r2.text()]);
 
-  const [t1, t2] = await Promise.all([r1.text(), r2.text()]);
-
-  res.json({
-    status1: r1.status,
-    status2: r2.status,
-    text1: t1.substring(0, 100),
-    text2: t2.substring(0, 100)
-  });
+    res.json({
+      status1: r1.status,
+      status2: r2.status,
+      text1: t1.substring(0, 200),
+      text2: t2.substring(0, 200)
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
